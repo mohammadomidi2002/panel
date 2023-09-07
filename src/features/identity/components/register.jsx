7514334ba4dde2633fbcbc1f1b9d72c58233cfd8
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-09-07 21:57:49 +0330

    fix register pages (set errors for input elements)

--- src/features/identity/components/register.jsx
+++ src/features/identity/components/register.jsx
@@ -8,12 +8,12 @@ const Register = () => {
     register,
     formState: { errors },
     handleSubmit,
-    watch
+    watch,
   } = useForm();
 
   const submit = (data) => {
-    console.log(data)
-  }
+    console.log(data);
+  };
   return (
     <>
       <div className="text-center mt-4">
@@ -36,39 +36,70 @@ const Register = () => {
             <form onSubmit={handleSubmit(submit)}>
               <div className="mb-3">
                 <label className="form-label">موبایل</label>
-                <input {...register('mobile', {
-                  required: 'موبایل الزامی است',
-                  minLength : 11,
-                  maxLength : 11
-                })} className={`form-control form-control-lg ${errors.mobile && "is-invalid"}`} />
-                { errors.mobile && errors.mobile.type === 'required' && (<p className="text-danger small fw-bolder mt-1">
-                  {errors.mobile?.message}
-                </p>)}
+                <input
+                  {...register("mobile", {
+                    required: "موبایل الزامی است",
+                    minLength: 11,
+                    maxLength: 11,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.mobile && "is-invalid"
+                  }`}
+                />
+                {errors.mobile && errors.mobile.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.mobile?.message}
+                  </p>
+                )}
+                {errors.mobile && ( errors.mobile.type === "maxLength" || errors.mobile.type === "minLength") && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    موبایل باید 11 رقمی باشد
+                  </p>
+                )}
               </div>
               <div className="mb-3">
                 <label className="form-label">رمز عبور</label>
-                <input {...register('password', {
-                  required: 'رمز عبور الزامی است',
-                  minLength: 4,
-                })}
-                  className={`form-control form-control-lg mb-2 ${errors.password && "is-invalid"}`}
+                <input
+                  {...register("password", {
+                    required: "رمز عبور الزامی است",
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.password && "is-invalid"
+                  }`}
                   type="password"
                 />
+                {errors.password && errors.password.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.password?.message}
+                  </p>
+                )}
               </div>
               <div className="mb-3">
                 <label className="form-label">تکرار رمز عبور</label>
-                <input {...register('confirmPassword', {
-                  required: 'تکرار رمز عبور الزامی است',
-                  validate: value => {
-                    if (watch('password') !== value) {
-                      return 'عدم تطابق با رمز عبور'
-                    }
-                  }
-
-                })}
-                  className={`form-control form-control-lg mb-2 ${errors.confirmPassword && "is-invalid"}`}
+                <input
+                  {...register("confirmPassword", {
+                    required: "تکرار رمز عبور الزامی است",
+                    validate: (value) => {
+                      if (watch("password") !== value) {
+                        return "عدم تطابق با رمز عبور";
+                      }
+                    },
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.confirmPassword && "is-invalid"
+                  }`}
                   type="password"
                 />
+                 {errors.confirmPassword && errors.confirmPassword.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.confirmPassword?.message}
+                  </p>
+                )}
+                {errors.confirmPassword && errors.confirmPassword.type === "validate" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.confirmPassword?.message}
+                  </p>
+                )}
               </div>
               <div className="text-center mt-3">
                 <button type="submit" className="btn btn-lg btn-primary">
