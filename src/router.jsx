6f010f98818474f12b4e289c274f9143fa4d53ea
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-09-12 16:29:01 +0330

    fix actions in route

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import IdentityLayouts from "./layouts/identityLayouts";
 import Login from "./features/identity/components/login";
-import Register from "./features/identity/components/register";
+import Register, { registerAction } from "./features/identity/components/register";
 
 const router = createBrowserRouter([
-    { element: <IdentityLayouts/>,
-children: [
-    { path: "/", element: <Login /> },
-    { path: "register", element: <Register /> }
-]}
+  {
+    element: <IdentityLayouts />,
+    children: [
+      { path: "/", element: <Login /> },
+      { path: "register", element: <Register />, action: registerAction },
+    ],
+  },
 ]);
 
 export default router;
