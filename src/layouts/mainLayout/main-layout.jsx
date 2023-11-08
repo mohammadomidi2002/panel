9d5a2e6664ee33174204d304202211caf89f0ca3
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-11-08 12:47:07 +0330

    fix mainLayout and refactor code

---
+++ src/layouts/mainLayout/main-layout.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Sidebar from './sidebar';
+import TopNav from './top-nav';
+import Footer from './footer';
+
+const MainLayout = () => {
+
+
+  const { t } = useTranslation()
+  return (
+    <div className='wrapper' style={{ minHeight: '100h' }}>
+      <Sidebar />
+      <div className='main'>
+        <TopNav />
+        <main className='content'>
+          <div className='container-fluid p-0'>
+            <Outlet />
+          </div>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ src/layouts/mainLayout/top-nav.jsx
@@ -0,0 +1,24 @@
+import ChangeLanguage from "../../components/change-language";
+import ChangeTheme from "../../components/change-them";
+import { useAppContext } from "../../context/app/app-context";
+
+const TopNav = () => {
+    const {toggleSidebar} = useAppContext();
+
+    return (
+        <nav className="navbar">
+          <a
+            className="sidebar-toggle"
+            onClick={toggleSidebar}
+          >
+            <i className="hamburger align-self-center"></i>
+          </a>
+          <div className="d-flex align-items-center gap-3  me-3">
+            <ChangeLanguage />
+            <ChangeTheme />
+          </div>
+        </nav>
+    )
+}
+
+export default TopNav;
