3b506b20a80f80504d950ce90de91086d41484c4
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-11-30 10:36:22 +0330

    fix pagination and fix spinner

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,12 +1,39 @@
 import _ from "lodash";
-const Pagination = ({totalRecords, pageSize = 10}) => {
-    const pages = Math.ceil(totalRecords / pageSize);
+import { useSearchParams } from "react-router-dom";
+const Pagination = ({ totalRecords, pageSize = 10 }) => {
+  const pages = Math.ceil(totalRecords / pageSize);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // plus (+) for convert string to number
+  const currentPage = +searchParams.get('page') || 1;
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setSearchParams({page: currentPage - 1})
+    }
+  }
+
+  const nextPage = () => {
+    if (currentPage < pages) {
+      setSearchParams({page: currentPage + 1})
+    }
+  }
   return (
     <nav>
-        <ul className="pagination pagination-lg">
-            <li className="page-item"><a className="page-link">قبلی</a></li>
-            <li className="page-item"><a className="page-link">بعدی</a></li>
-        </ul>
+      <ul className="pagination pagination-lg">
+        <li className={`page-item ${currentPage === 1 ? 'disable opacity-50' : ''}`} onClick={prevPage}>
+          <a className="page-link">قبلی</a>
+        </li>
+        {_.times(pages, index => (
+          <li key={`page${index + 1}`} className={`page-item ${index + 1 === currentPage ? 'active' : ''}`} onClick={() => setSearchParams({page: index + 1})}>
+            <a className="page-link">{index + 1}</a>
+          </li>
+        ))}
+        <li className={`page-item ${currentPage === pages ? 'disable opacity-50' : ''}`} onClick={nextPage}>
+          <a className="page-link">بعدی</a>
+        </li>
+      </ul>
     </nav>
   )
 }
