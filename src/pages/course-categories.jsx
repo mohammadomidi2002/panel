46c958e71da0dc1596a3a3b34804740ddc782126
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-12-02 16:12:39 +0330

    create modal component and use in category for delete

--- src/pages/course-categories.jsx
+++ src/pages/course-categories.jsx
@@ -1,31 +1,45 @@
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { httpInterceptedService } from "@core/https-service";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import CategoryList from "../features/categories/components/category-list";
+import Modal from "../components/modal";
 
 const CourseCategories = () => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
   const data = useLoaderData();
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="d-flex align-items-center justify-content-between mb-5">
-          <a className="btn btn-primary fw-bolder mt-n1">
-            افزودن دسته جدید
-          </a>
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="d-flex align-items-center justify-content-between mb-5">
+            <a className="btn btn-primary fw-bolder mt-n1">
+              افزودن دسته جدید
+            </a>
+          </div>
+          <Suspense fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}>
+            <Await resolve={data.categories}>
+              {
+                (loadedCategories) => <CategoryList setShowDeleteModal={setShowDeleteModal} categories={loadedCategories} />
+              }
+            </Await>
+          </Suspense>
         </div>
-        <Suspense fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}>
-          <Await resolve={data.categories}>
-            {
-              (loadedCategories) => <CategoryList categories={loadedCategories}/>
-            }
-          </Await>
-        </Suspense>
       </div>
-    </div>
+      <Modal isOpen={showDeleteModal} open={setShowDeleteModal} title="حذف" body="آیا از حذف این دسته اطمینان دارید؟">
+              <button className="btn btn-secondary fw-bolder" onClick={() => {
+                setShowDeleteModal(false)
+              }}>
+                انصراف
+              </button>
+              <button className="btn btn-primary fw-bolder">
+                ثبت
+              </button>
+      </Modal>
+    </>
   )
 }
 
-export async function categoriesLoader ({request}) {
+export async function categoriesLoader({ request }) {
   return defer({
     categories: loadCategories(request)
   })
