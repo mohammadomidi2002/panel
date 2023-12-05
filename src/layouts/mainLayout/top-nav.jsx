947cea9d8fd7cbbd8e01c2ec3e44467120ba6a56
mohammad omidi <mohammadomidi.developer@gmail.com>
2023-12-05 13:22:28 +0330

    fix logout in project and last commit

--- src/layouts/mainLayout/top-nav.jsx
+++ src/layouts/mainLayout/top-nav.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import ChangeLanguage from "../../components/change-language";
 import ChangeTheme from "../../components/change-them";
 import { useAppContext } from "../../context/app/app-context";
 
 const TopNav = () => {
     const {toggleSidebar} = useAppContext();
+    const navigate = useNavigate();
+    const {language} = useAppContext();
+
+    const logout = () => {
+      localStorage.removeItem('token');
+      navigate('/login');
+    }
 
     return (
         <nav className="navbar">
@@ -13,9 +21,14 @@ const TopNav = () => {
           >
             <i className="hamburger align-self-center"></i>
           </a>
-          <div className="d-flex align-items-center gap-3  me-3">
+          <div className="d-flex align-items-center gap-3 me-3">
             <ChangeLanguage />
             <ChangeTheme />
+          </div>
+          <div className={`${language === 'fa' ? 'me-auto' : 'ms-auto'}`}>
+            <button className="btn ms-2 btn-outline-danger fw-bolder" onClick={logout}>
+              خارج شوید
+            </button>
           </div>
         </nav>
     )
